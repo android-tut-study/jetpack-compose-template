27867b58f8df7f3e6404ad84e27b81ac997fdfdf
ANNX1 <xuanan290192@gmail.com>
2022-04-10 15:06:43 +0700

    Move resource
    Move app resource to common-ui module

---
+++ ui/ui-common/src/main/java/com/study/compose/ui/common/components/AppIcon.kt
@@ -0,0 +1,35 @@
+package com.study.compose.ui.common.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+
+@Composable
+fun AppIcon(
+    @DrawableRes resId: Int,
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = MaterialTheme.colors.primary
+) {
+    // for testing
+    val semantics = if (contentDescription != null) {
+        Modifier.semantics {
+            this.contentDescription = contentDescription
+            this.role = Role.Image
+        }
+    } else modifier
+
+    Box(modifier = modifier.then(semantics)) {
+        Icon(painter = painterResource(id = resId), contentDescription = null, tint = tint)
+    }
+}
