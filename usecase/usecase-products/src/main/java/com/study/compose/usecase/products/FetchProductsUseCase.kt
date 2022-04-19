c5ac9323e4bdf548fb244ccb492f37660f92cda4
ANNX1 <xuanan290192@gmail.com>
2022-04-19 16:18:17 +0700

    Add Fetch products usecase

---
+++ usecase/usecase-products/src/main/java/com/study/compose/usecase/products/FetchProductsUseCase.kt
@@ -0,0 +1,23 @@
+package com.study.compose.usecase.products
+
+import com.study.compose.core.dispatcher.CoroutineDispatchers
+import com.study.compose.core.domain.model.ProductDomain
+import com.study.compose.core.result.Result
+import com.study.compose.core.usecase.FlowUseCase
+import com.study.compose.domain.products.repository.ProductRepo
+import kotlinx.coroutines.flow.*
+
+class FetchProductsUseCase(
+    dispatcher: CoroutineDispatchers,
+    private val repo: ProductRepo
+) : FlowUseCase<Nothing, List<ProductDomain>>(dispatcher.io) {
+
+    override fun execute(params: Nothing): Flow<Result<List<ProductDomain>>> {
+        return flow {
+            repo.fetchProducts()
+                .onEach { emit(Result.Success(data = it)) }
+                .catch { emit(Result.Failure(throwable = it)) }
+                .collect()
+        }
+    }
+}
---
+++ usecase/usecase-products/src/main/java/com/study/compose/usecase/products/di/UseCaseModule.kt
@@ -0,0 +1,21 @@
+package com.study.compose.usecase.products.di
+
+import com.study.compose.core.dispatcher.CoroutineDispatchers
+import com.study.compose.domain.products.repository.ProductRepo
+import com.study.compose.usecase.products.FetchProductsUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+
+    @Provides
+    fun providesFetchProductUseCase(dispatchers: CoroutineDispatchers, productRepo: ProductRepo) =
+        FetchProductsUseCase(
+            dispatcher = dispatchers,
+            repo = productRepo
+        )
+}
