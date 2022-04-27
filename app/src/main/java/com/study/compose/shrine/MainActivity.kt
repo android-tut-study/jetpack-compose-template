08dbada98029a735a7c05e3f215078df52b40637
ANNX1 <xuanan290192@gmail.com>
2022-04-27 09:08:24 +0700

    Add shared app state viewmodel for main activity and home

--- app/src/main/java/com/study/compose/shrine/MainActivity.kt
+++ app/src/main/java/com/study/compose/shrine/MainActivity.kt
@@ -5,15 +5,23 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import com.study.compose.shrine.navigation.AppNavigation
 import com.study.compose.shrine.navigation.Screen
 import com.study.compose.ui.common.theme.ShrineComposeTheme
 import com.study.compose.ui.home.components.BottomCart
 import com.study.compose.ui.home.data.SampleCartItems
+import com.study.compose.ui.state.AppStateViewModel
+import com.study.compose.ui.state.rememberAppState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,26 +34,53 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    @Composable
-    fun AppContent() {
-        val navController = rememberNavController()
-        ShrineComposeTheme {
-            BoxWithConstraints(Modifier.fillMaxSize()) {
-                val currentRoute =
-                    navController.currentBackStackEntryFlow.collectAsState(initial = navController.currentBackStackEntry)
-                AppNavigation(
-                    navController = navController
-                )
+}
 
-                if (currentRoute.value?.destination?.route != Screen.Landing.route) {
-                    BottomCart(
-                        modifier = Modifier.align(Alignment.BottomEnd),
-                        maxHeight = maxHeight,
-                        maxWidth = maxWidth,
-                        carts = SampleCartItems
-                    )
-                }
-            }
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun AppContent() {
+    val appState = rememberAppState()
+    val navController = appState.navController
+    val viewModelStoreOwner = checkNotNull(LocalViewModelStoreOwner.current) {
+        "No ViewModelStoreOwner was provided via LocalViewModelStoreOwner"
+    }
+    val appStateViewModel = viewModel<AppStateViewModel>(viewModelStoreOwner = viewModelStoreOwner)
+
+    ShrineComposeTheme {
+        BoxWithConstraints(Modifier.fillMaxSize()) {
+            AppNavigation(
+                navController = navController,
+                viewModelStoreOwner = viewModelStoreOwner
+            )
+            BottomCart(
+                modifier = Modifier.align(Alignment.BottomEnd),
+                navController = navController,
+                appStateViewModel = appStateViewModel
+            )
         }
+    }
+}
+
+@Composable
+fun BottomCart(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    appStateViewModel: AppStateViewModel,
+) {
+    val config = LocalConfiguration.current
+    val currentRoute =
+        navController.currentBackStackEntryFlow.collectAsState(initial = navController.currentBackStackEntry)
+
+    val state by appStateViewModel.state.collectAsState()
+    if (currentRoute.value?.destination?.route != Screen.Landing.route) {
+        if (state.showBottomCart)
+            BottomCart(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .then(modifier),
+                maxHeight = config.screenHeightDp.dp,
+                maxWidth = config.screenWidthDp.dp,
+                carts = SampleCartItems
+            )
     }
 }
--- app/src/main/java/com/study/compose/shrine/navigation/AppNavigation.kt
+++ app/src/main/java/com/study/compose/shrine/navigation/AppNavigation.kt
@@ -1,12 +1,17 @@
 package com.study.compose.shrine.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.study.compose.ui.detail.Detail
 import com.study.compose.ui.home.HomeScreen
 import com.study.compose.ui.landing.LandingScreen
+import com.study.compose.ui.state.AppStateViewModel
 
 sealed class Screen(val route: String) {
     object Landing : Screen("landing")
@@ -29,8 +34,9 @@ sealed class ProductScreen(
 
 
 @Composable
-fun AppNavigation(navController: NavHostController) {
+fun AppNavigation(navController: NavHostController, viewModelStoreOwner: ViewModelStoreOwner) {
     NavHost(navController = navController, startDestination = Screen.Landing.route) {
+
         composable(Screen.Landing.route) {
             LandingScreen {
                 navController.navigate(Screen.Products.route, navOptions {
@@ -38,7 +44,7 @@ fun AppNavigation(navController: NavHostController) {
                 })
             }
         }
-        addProductTopLevel(navController = navController)
+        addProductTopLevel(navController = navController, viewModelStoreOwner = viewModelStoreOwner)
     }
 }
 
@@ -51,24 +57,34 @@ fun NavGraphBuilder.addShowProductDetail(
     }
 }
 
-fun NavGraphBuilder.addProducts(navController: NavController, root: Screen) {
+fun NavGraphBuilder.addProducts(
+    navController: NavController,
+    root: Screen,
+    viewModelStoreOwner: ViewModelStoreOwner
+) {
     composable(route = ProductScreen.Products.createRoute(root)) {
-        HomeScreen(
-            onProductSelect = { cart ->
-                navController.navigate(ProductScreen.ShowDetail.createRoute(root))
-            }
-        )
+        CompositionLocalProvider(
+            LocalViewModelStoreOwner provides viewModelStoreOwner
+        ) {
+            HomeScreen(
+                onProductSelect = { cart ->
+                    navController.navigate(ProductScreen.ShowDetail.createRoute(root))
+                },
+            )
+        }
+
     }
 }
 
 fun NavGraphBuilder.addProductTopLevel(
     navController: NavController,
+    viewModelStoreOwner: ViewModelStoreOwner,
 ) {
     navigation(
         route = Screen.Products.route,
         startDestination = ProductScreen.Products.createRoute(Screen.Products)
     ) {
-        addProducts(navController = navController, root = Screen.Products)
+        addProducts(navController = navController, root = Screen.Products, viewModelStoreOwner = viewModelStoreOwner)
         addShowProductDetail(navController = navController, root = Screen.Products)
     }
 }
