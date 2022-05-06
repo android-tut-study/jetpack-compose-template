f75e5de6caaf5ee5718f81379eabe4d258ac1123
ANNX1 <xuanan290192@gmail.com>
2022-05-06 18:01:37 +0700

    Update build.gradle.kts

--- includeBuild/gradleConfiguration/build.gradle.kts
+++ includeBuild/gradleConfiguration/build.gradle.kts
@@ -24,6 +24,6 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
     implementation("com.android.tools.build:gradle:7.1.2")
 }
