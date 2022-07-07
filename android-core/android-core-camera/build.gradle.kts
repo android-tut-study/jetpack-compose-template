c6b31ba045dabd3ce6eaebac638e4e3a2cb5d649
ANNX1 <xuanan290192@gmail.com>
2022-07-07 22:59:53 +0700

    add android-core-camera module

---
+++ android-core/android-core-camera/build.gradle.kts
@@ -0,0 +1,13 @@
+import com.example.Libs
+
+plugins {
+    id("app-plugin")
+}
+
+dependencies {
+    // CameraX
+    api(Libs.AndroidX.Camera.CAMERA2)
+    api(Libs.AndroidX.Camera.LIFECYCLE)
+    api(Libs.AndroidX.Camera.VIEW)
+    api(Libs.ZXING.CORE)
+}
