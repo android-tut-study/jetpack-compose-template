ddd1b16f0143ba01201659da47336965506621b6
ANNX1 <xuanan290192@gmail.com>
2022-07-11 15:08:55 +0700

    Remove redundant test dependencies
    Home, Landing, Cart

--- ui/ui-cart/build.gradle.kts
+++ ui/ui-cart/build.gradle.kts
@@ -19,8 +19,4 @@ dependencies {
 
     implementation(Libs.AndroidX.Paging.RUNTIME)
     implementation(Libs.AndroidX.Paging.COMPOSE)
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
