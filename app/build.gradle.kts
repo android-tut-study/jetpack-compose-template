a2519a2f5f1b507130104c6979196d41bf5ff751
ANNX1 <xuanan290192@gmail.com>
2022-08-06 23:24:27 +0700

    Update App build script

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,15 @@
 import com.study.compose.gradleconfiguration.util.uiModule
 
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(gradlePlugins.plugins.application)
-    alias(gradlePlugins.plugins.kotlin)
-    kotlin("kapt")
-    id(gradlePlugins.hilt.app.plugin.get().module.name)
     id("app-plugin")
+    id("hilt-plugin")
+}
+
+hiltConfiguration {
+    version.set(hiltLibs.versions.hilt)
+    navigation {
+        composeVersion.set(hiltLibs.versions.navigationCompose)
+    }
 }
 
 kapt {
@@ -31,17 +34,9 @@ dependencies {
     implementation(androidx.navigation.compose)
     implementation(androidx.appcompat)
 
-    implementation(hiltLibs.hilt.android)
-    kapt(hiltLibs.hilt.android.compiler)
-    implementation(hiltLibs.hilt.navigation.compose)
-
     // LifeCycle
     implementation(androidx.lifecycle.viewmodel.compose)
     implementation(androidx.lifecycle.livedata.ktx)
-
-    // Hilt
-    implementation(hiltLibs.hilt.android)
-    kapt(hiltLibs.hilt.compiler)
 
     implementation(io.coil.core)
 }
